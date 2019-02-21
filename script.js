9522948f9f83f3b29664943657eaba8c6f7acada
AzRun <qmartine@student.42.fr>
2019-02-21 16:24:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+var img = document.createElement("img");
+img.src = "https://www.eni-ecole.fr/Content/Images/Static/logo-eni.png";
+img.style = 'display: block; margin-left: auto; margin-right: auto;';
+document.body.appendChild(img);
+var p = document.createElement("p");
+p.innerHTML = 'PLop';
+p.style.foreColor = 'green';
+document.body.appendChild(p);
+document.body.bgColor="Black";
+
+</script>
